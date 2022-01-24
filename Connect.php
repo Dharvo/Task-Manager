17e9c26906287ff9949d357769cf4997aada107d
David <86372079+Dharvo@users.noreply.github.com>
2022-01-24 14:59:18 +0100

    Update Connect.php

--- Connect.php
+++ Connect.php
@@ -1,7 +1,9 @@
 <?php
 //Create Connection
-	$conn = mysqli_connect("localhost", "root", "", "ittaskmanager");
-		// Check connection
+
+// 	$conn = mysqli_connect("localhost", "root", "", "ittaskmanager");
+$conn = mysqli_connect("us-cdbr-east-05.cleardb.net", "b22259f703577d", "acb562b9", "heroku_f2f9053ce56406f");
+// Check connection
 	if($conn === false){
         //If Connection Fails 
 	die("ERROR: Could not connect. Please check-". mysqli_connect_error());
